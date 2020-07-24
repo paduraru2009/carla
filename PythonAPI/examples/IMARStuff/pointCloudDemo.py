9d96651e52e1079ef66c5828ac6f4081cf8a1b17
paduraru2009 <ciprian.paduraru2009@gmail.com>
2020-07-24 18:07:04 +0300

    demo for skinned mesh

--- PythonAPI/examples/IMARStuff/pointCloudDemo.py
+++ PythonAPI/examples/IMARStuff/pointCloudDemo.py
@@ -1,141 +1,24 @@
 import os
 import numpy as np
 import open3d as o3d
+from pytransform3d import rotations
+import copy
+import pytransform3d as py3d
 
-print("Let's draw a cubic using o3d.geometry.LineSet.")
-points = [
-        [0, 0, 0],
-        [1, 0, 0],
-        [0, 1, 0],
-        [1, 1, 0],
-        [0, 0, 1],
-        [1, 0, 1],
-        [0, 1, 1],
-        [1, 1, 1],
-]
-lines = [
-        [0, 1],
-        [0, 2],
-        [1, 3],
-        [2, 3],
-        [4, 5],
-        [4, 6],
-        [5, 7],
-        [6, 7],
-        [0, 4],
-        [1, 5],
-        [2, 6],
-        [3, 7],
-]
-colors = [[1, 0, 0] for i in range(len(lines))]
-line_set = o3d.geometry.LineSet(
-    points=o3d.utility.Vector3dVector(points),
-    lines=o3d.utility.Vector2iVector(lines),
-)
-line_set.colors = o3d.utility.Vector3dVector(colors)
-o3d.visualization.draw_geometries([line_set])
-
-exit(0)
-
+def rotateAroundAxis(axis, degrees):
+    euler = [0, 0, 0]
+    euler[axis] = degrees
+    R = py3d.rotations.matrix_from_euler_xyz(euler)
+    return R
 
 basePath = "C:/Users/Ciprian/OneDrive - University of Bucharest, Faculty of Mathematics and Computer Science/IMAR_Work/New folder"
-Waymo_X_orig = os.path.join(basePath, "combined_carla_moving - Copy.ply")
-Waymo_X = os.path.join(basePath, "combined_carla_moving - Copy_conv.ply")
-
-# Given a dictionary of [3d points from the cloud] -> {rgb segmentation label , raw segmentation label}, save it to the specified file
-# (Taken from CARLA's code)
-def save_3d_pointcloud(points_3d, filename):
-    """Save this point-cloud to disk as PLY format."""
-
-    def construct_ply_header():
-        """Generates a PLY header given a total number of 3D points and
-        coloring property if specified
-        """
-        points = len(points_3d)  # Total point number
-        header = ['ply',
-                  'format ascii 1.0',
-                  'element vertex {}',
-                  'property float32 x',
-                  'property float32 y',
-                  'property float32 z',
-                  'property uchar red',
-                  'property uchar green',
-                  'property uchar blue',
-                  'property uchar label',
-                  'end_header']
-        return '\n'.join(header).format(points)
-
-    for point in points_3d:
-        label = point[6]
-        for p in point:
-            try:
-                n = float(p)
-            except ValueError:
-                print ("Problem " + str(point))
-    # points_3d = np.concatenate(
-    #     (point_list._array, self._color_array), axis=1)
-    try:
-        ply = '\n'.join(
-            ['{:.2f} {:.2f} {:.2f} {:.0f} {:.0f} {:.0f} {:.0f}'.format(*p) for p in points_3d])  # .replace('.',',')
-    except ValueError:
-        for point in points_3d:
-            print (point)
-            print ('{:.2f} {:.2f} {:.2f} {:.0f} {:.0f} {:.0f} {:.0f}'.format(*point))
-    # Create folder to save if does not exist.
-    # folder = os.path.dirname(filename)
-    # if not os.path.isdir(folder):
-    #     os.makedirs(folder)
-
-    # Open the file and save with the specific PLY format.
-    with open(filename, 'w+') as ply_file:
-        ply_file.write('\n'.join([construct_ply_header(), ply]))
-
-def do_conversion_toO3D(srcPath, targetPath):
-    from plyfile import PlyData, PlyElement
-    plydata = PlyData.read(srcPath)
-    nr_points = plydata.elements[0].count
-    pointcloud3D = np.array([plydata['vertex'][k] for k in range(nr_points)])
-    newpointcloud3d = []
-    for point in pointcloud3D:
-        x,y,z,r,g,b, label = point
-
-        """
-        r /= 255.0
-        g /= 255.0
-        b /= 255.0
-        """
-
-        newpointcloud3d.append((x, y, z, r, g, b, label))
-
-    save_3d_pointcloud(newpointcloud3d, targetPath)
+#Waymo_X_orig = os.path.join(basePath, "combined_carla_moving - Copy.ply")
+Waymo_X = os.path.join(basePath, "combined_carla_moving - Copy_conv_2.ply")#"combined_carla_moving - Copy_conv.ply")
 
 
-#do_conversion_toO3D(Waymo_X_orig, Waymo_X)
-
-def do_conv_folder(basePath, START_FRAME, END_FRAME, doSegToo = False):
-    for i in range(START_FRAME, END_FRAME+1):
-        print("## convering frame ", i)
-        filename_in = os.path.join(basePath, "combined_carla_moving_f{0:05d}.ply".format(i))
-        filename_out = os.path.join(basePath, "combined_carla_moving_f{0:05d}_conv.ply".format(i))
-
-        print("RGB..")
-        if not os.path.exists(filename_out):
-            do_conversion_toO3D(filename_in, filename_out)
-
-        print("seg..")
-        if doSegToo:
-            filename_in = os.path.join(basePath, "combined_carla_moving_segColor_f{0:05d}.ply".format(i))
-            filename_out = os.path.join(basePath, "combined_carla_moving_segColor_f{0:05d}_conv.ply".format(i))
-
-            if not os.path.exists(filename_out):
-                do_conversion_toO3D(filename_in, filename_out)
-
-
-sceneBasePath = "C:/Users/Ciprian/OneDrive - University of Bucharest, Faculty of Mathematics and Computer Science/IMAR_Work/New folder/Scene18311"
-do_conv_folder(sceneBasePath, 0, 197, doSegToo=True)
-
-if False:
+if True:
     import open3d as o3d
+
 
     print("Load a ply point cloud, print it, and render it")
     pcd = o3d.io.read_point_cloud(Waymo_X)
@@ -143,4 +26,39 @@ if False:
     print(np.asarray(pcd.points))
     print(np.asarray(pcd.colors))
     print(pcd.has_colors())
-    o3d.visualization.draw_geometries([pcd])
+
+    R = rotateAroundAxis(0, np.pi)
+    pcd = pcd.rotate(R, center=np.array([0, 0, 0]))
+    #pcd = pcd.scale(SCALE_FACTOR, center=np.array([0, 0, 0]))
+
+    print("Testing IO for meshes ...")
+
+    objmesh_path = os.path.join(basePath, "sample_mesh.obj")
+    skinned_mesh = o3d.io.read_triangle_mesh(objmesh_path)
+    skinned_mesh.compute_vertex_normals()
+    skinned_mesh.paint_uniform_color([0.1, 0.1, 0.7])
+    skinned_mesh.scale(scale=4.0, center = skinned_mesh.get_center())
+
+
+    Rx_mesh = rotateAroundAxis(0, np.pi/2)
+    Ry_mesh = rotateAroundAxis(2, np.pi/2)
+    skinned_mesh = skinned_mesh.rotate(Rx_mesh, center=skinned_mesh.get_center())
+    skinned_mesh = skinned_mesh.rotate(Ry_mesh, center=skinned_mesh.get_center())
+    skinned_mesh.translate([15, 0, -11])
+
+    mesh_vertices = np.asarray(skinned_mesh.vertices)
+    mesh_triangles = np.asarray(skinned_mesh.triangles)
+    mesh_colors = np.asarray(skinned_mesh.vertex_colors)
+
+    #print(mesh)
+
+    skinnedmesh_sampled = skinned_mesh.sample_points_uniformly(number_of_points=800)
+    skinnedmesh_sampled.translate([15, 0, 0])
+    skinnedmesh_sampled.paint_uniform_color([0.0, 0.0, 0.1])
+    skinnedmesh_sampled_bbox = skinnedmesh_sampled.get_axis_aligned_bounding_box()
+    o3d.visualization.draw_geometries([pcd, skinned_mesh, skinnedmesh_sampled, skinnedmesh_sampled_bbox])
+
+
+    #o3d.visualization.draw_geometries([mesh_cylinder, mesh_frame, mesh_box, mesh_sphere
+    #                                   , mesh
+    #                                   ])
